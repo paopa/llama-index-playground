c9d4f28d0105750771cda1e11eb85d13d7688543
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-09-21 01:23:46 +0800

    feat: basic agent case

---
+++ case_basic_agent/__init__.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+from llama_index.core.tools import FunctionTool
+
+load_dotenv()
+
+
+def multiply(a: float, b: float) -> float:
+    """Multiply two numbers and returns the product"""
+    return a * b
+
+
+multiply_tool = FunctionTool.from_defaults(fn=multiply)
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers and returns the sum"""
+    return a + b
+
+
+add_tool = FunctionTool.from_defaults(fn=add)
+
+llm = OpenAI(model="gpt-4o-mini", temperature=0)
+agent = ReActAgent.from_tools([multiply_tool, add_tool], llm=llm, verbose=True)
+
+response = agent.chat("What is 20+(2*4)? Use a tool to calculate every step.")
